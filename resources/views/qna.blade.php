613ea1d1061a52802edc64c6f61364bdbbafdd27
zayrats <sugarprasetya@gmail.com>
2025-06-17 14:01:28 +0700

    update font on qna

--- resources/views/qna.blade.php
+++ resources/views/qna.blade.php
@@ -10,7 +10,7 @@
     {{-- Form Pencarian --}}
     <form action="{{ route('questions.search') }}" method="GET" class="flex items-center gap-2 mb-6">
         <input type="text" name="q" value="{{ request('q') }}" placeholder="Cari topik..."
-               class="p-2 rounded-md border border-gray-300 w-64 focus:outline-none focus:ring-2 focus:ring-blue-500">
+               class="p-2 rounded-md border border-gray-300 w-64 focus:outline-none focus:ring-2 focus:ring-blue-500 font-black">
         <button type="submit" class="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">Cari</button>
     </form>
 
